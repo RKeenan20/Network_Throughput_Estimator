23cd67f5cca2f9eaabbc48e98e09f22b4a51078a
Robert Keenan <robertmkeenan1@gmail.com>
2020-02-23 12:09:37 +0000

    802.11a/g working for both TCP and UDP. Need to clean methods

--- func.py
+++ func.py
@@ -1,6 +1,27 @@
+from operator import itemgetter
+
+
+def getList(dict):
+    return list(map(itemgetter(0), dict.items()))
 def print_out_a_choice(list):
     for i in range(len(list)):
-        print(str(i+1) + ":" + list[i] + "\n")
+            print(str(i+1) + ": " + list[i] + "\n")
+
+def print_dict(dict, value_1):
+    if value_1 is None:
+        for i, value in enumerate(dict.keys()):
+            print(i+1, value)
+
+    else:
+        for i, value in enumerate(value_1["Data Rates"]):
+            print(str(i + 1) + ": " + str(value) + "Mbps")
+
+
+
+    #chosen_number = input("Enter choice in numerical form: \n")
+    #chosen_value = ((list_array[int(chosen_number)-1])[0])
+    #return chosen_value.split(":")[1]
+
     #number_input = input("Enter choice in numerical form: \n")
     #print(type(list))
     #chosen_answer = str(list[number_input-1])
--- main.py
+++ main.py
@@ -12,85 +12,88 @@ CTS_SIZE = 14  # bytes
 
 # TAIL_BITS_APPENDED = 6 #bits
 
-
+#FIXME for the best case rate, the data rate entered by the person will always have the same NBits and CRate but the NChan
+# will change between the bandwidths and as a result, we can just do (NBits * CRate * NChan) * Nss
 def main():
-    with open('standards.json') as json_file:
-        config = json.loads(json_file.read())
-        print("Choose a protocol from the following: \n")
-        func.print_out_a_choice(config["protocols"])
-        print(config['protocols'][0])
-        # Check whether the relevant protocol has been entered
-        while True:
-            chosen_protocol = config['protocols'][int(input("Enter choice in numerical form: \n")) - 1]
-            if chosen_protocol in config["protocols"]:
-                break
-            else:
-                continue
-        print("Please choose a relevant 802.11 Standard from the below list:")
-        func.print_out_a_choice(config["standards"])
-        while True:
-            chosen_standard = config['standards'][input("Enter choice in numerical form: \n") - 1]
-            if chosen_standard in config['standards']:
-                break
-            else:
-                continue
-        # Now that I have the input from the user, we need to see what type of data rate they want to use
-        print("Choose a data rate from the following list:")
-        # This will print out the "Data Rates" list of the chosen standard which is stored as a string
-        func.print_out_a_choice(config[chosen_standard]["Data Rates"])
-        while True:
-            chosen_data_rate = config[chosen_standard]["Data Rates"][input("Enter choice in numerical form: \n") - 1]
-            if chosen_data_rate in config[chosen_standard]["Data Rates"]:
-                break
-            else:
-                continue
-        chosen_data_rate_info = config[chosen_standard]["Data Rate Encoding Data"][
-            config[chosen_standard]["Data Rates"].indexof(chosen_data_rate)]
-        MAC_header = config[chosen_standard]["MAC Header"]
-        SIFS = config[chosen_standard]["SIFS"]
-        Preamble = config[chosen_standard]["Preamble"]
-        if config[chosen_standard] is "802.11g":
-            signal_extension = config[chosen_standard]["Signal Extension"]
+    print("Choose a protocol from the following: \n")
+    func.print_out_a_choice(config.protocol)
+    while True:
+        chosen_protocol = config.protocol[int(input("Enter choice in numerical form: \n")) - 1]
+        if chosen_protocol in config.protocol:
+            break
         else:
-            signal_extension = 0
+            continue
+    print("Please choose a relevant 802.11 Standard from the below list:")
+    func.print_dict(config.standards, None)
 
+    while True:
+        chosen_standard = list(config.standards)[int(input("Enter choice in numerical form: \n")) - 1]
+        if chosen_standard in config.standards:
+            break
+        else:
+            continue
+    # Now that I have the input from the user, we need to see what type of data rate they want to use
+    print("Choose a data rate from the following list:")
+    # This will print out the "Data Rates" list of the chosen standard which is stored as a string
+    func.print_dict(config.standards[chosen_standard]["Data Rates"], config.standards[chosen_standard])
+    while True:
+        chosen_data_rate = list(config.standards[chosen_standard]["Data Rates"])[int(input("Enter choice in numerical form: \n")) - 1]
+        if chosen_data_rate in config.standards[chosen_standard]["Data Rates"]:
+            break
+        else:
+            continue
+
+
+    chosen_data_rate_info = list(config.standards[chosen_standard]["Data Rate's Data"])[
+        list(config.standards[chosen_standard]["Data Rates"]).index(chosen_data_rate)]
+    MAC_header = config.standards[chosen_standard]["MAC Header"]
+    SIFS = config.standards[chosen_standard]["SIFS"]
+    Preamble = config.standards[chosen_standard]["Preamble"]
+    print(chosen_standard)
+    if str(chosen_standard).split('\n')[0] == "802.11g":
+        signal_extension = 6
+    else:
+        signal_extension = 0
+
+
+    #FIXME this is where the indentation happened
     # I now have the chosen_data_rate and the chosen standards
     # I can now process the data rate values
 
     # Gathering all of the relevant information for the calculation
     NBits = chosen_data_rate_info[0]
-    CRate = chosen_data_rate[1]
+    CRate = chosen_data_rate_info[1]
     NChan = chosen_data_rate_info[2]
     SDur = chosen_data_rate_info[3]
     DIFS = (2 * SLOT_TIME) + SIFS
-    # FIXME should be multiplied by Nss once we have conluded what the answer to that is
-    bits_per_OFDM_symbol = NBits * CRate * NChan
 
+    # FIXME should be multiplied by Nss once we have concluded what the answer to that is
+    bits_per_ofdm_symbol = NBits * CRate * NChan
+    print(ceil(((RTS_SIZE * 8) + 6) / bits_per_ofdm_symbol), SDur)
     total_packet_size = PACKET_SIZE + MAC_header + SNAP_LLC  # bytes
 
     # at the data rate, I can see how many bits are encoded by one symbol
-    RTS_Dur = ceil(((RTS_SIZE * 8) + 6) / bits_per_OFDM_symbol) * SDur
-    CTS_Dur = ceil(((CTS_SIZE * 8) + 6) / bits_per_OFDM_symbol) * SDur
-    Data_Frame_Dur = ceil(((total_packet_size * 8) + 6) / bits_per_OFDM_symbol) * SDur
-    ACK_Dur = SDur
-    TCP_ACK_Dur = ceil(((TCP_ACK_SIZE * 8) + 6) / bits_per_OFDM_symbol)
+    RTS_Dur = ceil(((RTS_SIZE * 8) + 6) / bits_per_ofdm_symbol) * SDur + signal_extension
+    CTS_Dur = ceil(((CTS_SIZE * 8) + 6) / bits_per_ofdm_symbol) * SDur + signal_extension
+    Data_Frame_Dur = ceil(((total_packet_size * 8) + 6) / bits_per_ofdm_symbol) * SDur + signal_extension
+    ACK_Dur = ceil(((14 * 8) + 6) / bits_per_ofdm_symbol) * SDur + signal_extension
+    TCP_ACK_Dur = ceil((((TCP_ACK_SIZE + MAC_header + SNAP_LLC) * 8) + 6) / bits_per_ofdm_symbol) * SDur + signal_extension
+    print(RTS_Dur, CTS_Dur, Data_Frame_Dur, ACK_Dur, TCP_ACK_Dur)
 
     transmission_dur = DIFS + Preamble + RTS_Dur + SIFS + Preamble + CTS_Dur + SIFS + Preamble + Data_Frame_Dur + SIFS + Preamble + ACK_Dur
-    if chosen_protocol.isequals("TCP"):
-        transmission_dur = transmission_dur + DIFS + Preamble + RTS_Dur + SIFS + Preamble + CTS_Dur + SIFS + Preamble + TCP_ACK_Dur + SIFS + Preamble + ACK_Dur
+    if chosen_protocol == "TCP":
+        transmission_dur += DIFS + Preamble + RTS_Dur + SIFS + Preamble + CTS_Dur + SIFS + Preamble + TCP_ACK_Dur + SIFS + Preamble + ACK_Dur
 
-    bits_per_second = (PACKET_SIZE * 8) / transmission_dur  # This result is given in Mbps
+    throughput = (PACKET_SIZE * 8) / transmission_dur  # This result is given in Mbps
 
     # 10GB of data can be written as 10e3 with reference to the bits per second which is in Mb
     data_test = 10e3 * 8  # in bits
 
     # Time taken to transfer 10GB of data
-    time_taken = data_test / bits_per_second
+    time_taken = data_test / throughput
 
-    print(
-        "The actual throughput for Protocol: " + chosen_protocol + " Standard: " + chosen_standard + " Data Rate: " + chosen_data_rate + "Mbps is: " +
-        bits_per_second+"\n")
-    print("The time taken to transfer 10GB of data is: "+time_taken + "\n")
+    print("Results are as follows: \nProtocol: " + chosen_protocol + "\nStandard: " + str(chosen_standard) + "\nData Rate: %d Mbps\nThroughput: %.3f Mbps\n" % (chosen_data_rate, throughput ))
+    print("The time taken to transfer 10GB of data is: %.2f seconds or %.2f minutes\n" % (time_taken, time_taken/60))
 
 
 if __name__ == "__main__":
